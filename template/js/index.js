48142cc81a266ec5e703b8d4e82e62be751bc05b
Segun Matthews <segunmatthews@outlook.com>
2017-11-17 06:36:30 +0100

    add index, signin, signup,  explore pages and plugins

---
+++ template/js/index.js
@@ -0,0 +1,7 @@
+// $('.carousel.carousel-slider').carousel({fullWidth: true});
+// $(document).ready(function(){
+//     $('.slider').slider();
+//   });
+$(document).ready(function(){
+    $('#modal1').modal();
+  });
