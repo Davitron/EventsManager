b7d67dff8a339c873f51dbe8198047e55a984027
Segun Matthews <segunmatthews@outlook.com>
2017-11-18 14:36:54 +0100

    add userDashboard.html to pages

--- template/js/index.js
+++ template/js/index.js
@@ -4,4 +4,26 @@
 //   });
 $(document).ready(function(){
     $('#modal1').modal();
+    $('#modalEvents').modal();
+    $('#create').modal();
+    $('#Edit').modal();
+    $('#delete').modal();
+    $(".dropdown-button").dropdown(
+      {
+        belowOrigin: true
+      }
+    );
+    $('ul.tabs').tabs();
+
+    $('.scrollspy').scrollSpy();
+    $(".button-collapse").sideNav();
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: false // Close upon selecting a date,
+    });
+  
   });
